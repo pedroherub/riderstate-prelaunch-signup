20c93ca5d576b58e99ad445d35b2987f4dd35492
Pedro Hernández <pedroherub@gmail.com>
2012-06-19 15:57:50 +0200

    Release fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -117,7 +117,7 @@ namespace :deploy do
       mkdir -p #{latest_release}/tmp &&
       ln -s #{shared_path}/log #{latest_release}/log &&
       ln -s #{shared_path}/system #{latest_release}/public/system &&
-      ln -s #{shared_path}/pids #{latest_release}/tmp/pids &&
+      ln -s #{shared_path}/tmp/pids #{latest_release}/tmp/pids &&
       ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml
     CMD
 
